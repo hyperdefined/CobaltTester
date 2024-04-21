364dd2033cfe326cad27f3a638fa548de6574a2d
hyperdefined <contact@hyper.lol>
2024-04-21 18:27:32 -0400

    what was I smoking

--- src/main/java/lol/hyper/cobalttester/CobaltTester.java
+++ src/main/java/lol/hyper/cobalttester/CobaltTester.java
@@ -89,7 +89,7 @@ public class CobaltTester {
                 cache.put(instance.toJSON());
                 frontEnd = "<a href=\"https://" + instance.frontEnd() + "\">" + instance.frontEnd() + "</a>";
             }
-            String api = "<a href=\"https://" + instance.api() + "/api/serverInfo\">" + instance.frontEnd() + "</a>";
+            String api = "<a href=\"https://" + instance.api() + "/api/serverInfo\">" + instance.api() + "</a>";
             String version = instance.version();
             String commit = instance.commit();
             String branch = instance.branch();
