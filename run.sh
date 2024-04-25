286b5f7936cd94bfc8b2c2992a8e35ad2d95152d
hyperdefined <contact@hyper.lol>
2024-04-25 18:49:09 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ cd /home/hyper/CobaltTester/ || exit
 java -jar CobaltTester-1.0-SNAPSHOT.jar
 bundle exec jekyll build
 cp instances.json _site
-sudo rm -r /var/www/instances.hyper.lol/_site
-sudo mv _site /var/www/instances.hyper.lol/
+sudo rm -r /var/www/instances.hyper.lol
+sudo mv _site/ /var/www/instances.hyper.lol
 sudo chown -R nginx:nginx /var/www/instances.hyper.lol/
