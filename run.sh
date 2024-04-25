bca8eca2fd270a1fa083796e76c006158c01c8f7
hyperdefined <contact@hyper.lol>
2024-04-25 16:02:53 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 export GEM_HOME="$HOME/gems"
 export PATH="$HOME/gems/bin:$PATH"
 cd /home/hyper/CobaltTester/ || exit
-rm -r _site
 java -jar CobaltTester-1.0-SNAPSHOT.jar
 bundle exec jekyll build
 cp instances.json _site
