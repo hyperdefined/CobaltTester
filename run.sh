488361a76a59fa54d639c90cc29ca653e97b6f41
hyperdefined <contact@hyper.lol>
2024-04-27 18:26:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export GEM_HOME="$HOME/gems"
 export PATH="$HOME/gems/bin:$PATH"
-cd /home/hyper/CobaltTester/ || exit
+cd /home/hyper/CobaltTester || exit
 java -jar CobaltTester-1.0-SNAPSHOT.jar
+cd /home/hyper/CobaltTester/web || exit
 bundle exec jekyll build
-cp instances.json _site
+cp /home/hyper/CobaltTester/backend/instances.json _site
 sudo rm -r /var/www/instances.hyper.lol
-sudo mv _site/ /var/www/instances.hyper.lol
+sudo mv /home/hyper/CobaltTester/web/_site/ /var/www/instances.hyper.lol
 sudo chown -R nginx:nginx /var/www/instances.hyper.lol/
